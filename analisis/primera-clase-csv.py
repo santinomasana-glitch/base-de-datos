314709b8e676613fd401d45a68b829fec1567eab
luanalopez20 <luanaslopez@outlook.com>
2026-06-04 12:01:30 -0300

    waza

--- analisis/primera-clase-csv.py
+++ analisis/primera-clase-csv.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #importando csv
 df=pd.read_csv("cumulative.csv")
@@ -40,3 +42,60 @@ elif suma_dinero > 200:
 else:
     print("estado:volumen de mercado bajo dentro del parametro")
     print("no se requiere accion adicional")
+
+#-------------------------------------------
+#GRAFICO 1: grafico de barras  (con seaborn)
+#-------------------------------------------
+
+print("\n generando grafico de barras")
+
+sns.set_theme(style="whitegrid")
+
+plt.figure(figsize=(9,5))
+
+sns.barplot(
+    data=df,
+    x="kepid",
+    y="koi_pdisposition",
+    estimator=sum,
+    errorbar=None,
+    palette="viridis"
+)
+
+plt.title(
+    "distribucion economica de tecnologia avanzada",fontsize=11
+)
+plt.xlabel("tipo de kepid", fontsize=11)
+plt.ylabel("total:(CANDI)", fontsize=11)
+
+plt.tight_layout()
+#plt.xticks(rotation=40, fontsize=6) 
+#sirve para rotar las letras
+plt.savefig("grafico_barras.png", dpi=300)
+plt.close()
+print("grafico de barras guardado exitosamente")
+
+#--------------------------------------------
+#grafico de torta
+#--------------------------------------------
+print("\n generando grafico de torta")
+datos_torta=(
+     df.groupby ("koi_pdisposition")["kepid"]
+    .sum()
+    .nlargest(5)
+)
+plt.figure(figsize=(7,7))
+plt.pie(
+   datos_torta.values,
+   labels=datos_torta.index,
+   autopct="%1.1f%%",
+   colors=sns.color_palette("Set2")[0:5],
+   startangle=140,
+   wedgeprops={'edgecolor':'white','linewidth':2},
+)
+plt.title(
+    "distribucion interna: tecnologia avanzada",fontsize=11
+)
+plt.tight_layout()
+plt.savefig("grafico_torta.png", dpi=300)
+print("grafico de torta guardado exitosamente")
